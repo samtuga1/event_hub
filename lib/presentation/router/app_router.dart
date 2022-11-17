7787d745bf86ee748a272d0ae9959299179cbad4
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-11-17 18:54:36 +0000

    update

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -2,9 +2,11 @@ import 'package:event_hub/presentation/screens/inner_screens/event_detail_screen
 import 'package:event_hub/presentation/screens/not_found_screen.dart';
 import 'package:event_hub/presentation/screens/onboarding/interest_screen.dart';
 import 'package:event_hub/presentation/screens/onboarding/fill_info_screen.dart';
+import 'package:event_hub/presentation/screens/inner_screens/ticket.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:event_hub/presentation/screens/onboarding/map_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../screens/bottom_nav_screen.dart';
 import '../screens/onboarding/auth_screen.dart';
@@ -24,18 +26,32 @@ class AppRouter {
                 );
               }),
             ],
+            footerBuilder: (context, action) {
+              return Text(
+                'By signing in you agree to our terms and agreements',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 19.sp,
+                  color: Colors.black45,
+                  fontFamily: 'Gilroy',
+                ),
+                textAlign: TextAlign.center,
+              );
+            },
           ),
         );
       case FillInfoScreen.routeName:
         return route(const FillInfoScreen());
+      case Ticket.routeName:
+        return route(const Ticket());
+      case BottomBarScreen.routeName:
+        return route(const BottomBarScreen());
       case AuthScreen.routeName:
         return route(const AuthScreen());
       case InterestScreen.routeName:
         return route(const InterestScreen());
       case MapScreen.routeName:
         return route(const MapScreen());
-      case BottomBarScreen.routeName:
-        return route(BottomBarScreen());
       case EventDetail.routeName:
         return route(const EventDetail());
       default:
---
+++ lib/presentation/widgets/upcoming_events.dart
@@ -0,0 +1,86 @@
+import 'package:event_hub/presentation/screens/inner_screens/ticket.dart';
+import 'package:flutter/material.dart';
+
+class UpcomingEvents extends StatelessWidget {
+  const UpcomingEvents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return ListView.separated(
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () => Navigator.of(context).pushNamed(
+          Ticket.routeName,
+        ),
+        child: Container(
+          color: Colors.transparent,
+          padding:
+              EdgeInsets.only(left: 20, right: 20, top: index == 0 ? 20 : 0),
+          child: Row(children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                color: Colors.amber,
+                width: 120,
+                height: 80,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Wine Tasting',
+                  style: theme.textTheme.headline2,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Date',
+                          style: theme.textTheme.labelMedium,
+                        ),
+                        Text(
+                          'Fri June 8, 2022',
+                          style: theme.textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Time',
+                          style: theme.textTheme.labelMedium,
+                        ),
+                        Text(
+                          '7:45AM',
+                          style: theme.textTheme.bodyText1,
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )
+          ]),
+        ),
+      ),
+      separatorBuilder: (context, index) => const Divider(
+        thickness: 0.5,
+      ),
+      itemCount: 10,
+    );
+  }
+}
