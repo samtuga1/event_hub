795dbda6a005f165fe2c87b1ecfc067e3aa77930
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-11-17 18:54:31 +0000

    update

---
+++ lib/presentation/screens/outer_screens/tickets_screen.dart
@@ -0,0 +1,62 @@
+import 'package:event_hub/presentation/widgets/past_events.dart';
+import 'package:event_hub/presentation/widgets/upcoming_events.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TicketsScreen extends StatefulWidget {
+  const TicketsScreen({super.key});
+
+  @override
+  State<TicketsScreen> createState() => _MyStatefulWidgetState();
+}
+
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _MyStatefulWidgetState extends State<TicketsScreen>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        title: Text(
+          'My Tickets',
+          style: theme.textTheme.headline2?.copyWith(fontSize: 28.sp),
+        ),
+        bottom: TabBar(
+          indicatorColor: theme.primaryColor,
+          labelStyle: theme.textTheme.headline2?.copyWith(
+            fontSize: 19.sp,
+            fontWeight: FontWeight.w700,
+          ),
+          labelColor: Colors.black,
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              text: 'Upcoming',
+            ),
+            Tab(
+              text: 'Past',
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          UpcomingEvents(),
+          PastEvents(),
+        ],
+      ),
+    );
+  }
+}
